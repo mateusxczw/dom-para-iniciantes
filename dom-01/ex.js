f4e0679b90e3835081be0e7a13c0fffe592af922
Mateus Cardozo <mateusczdev@gmail.com>
2024-11-07 21:37:18 -0300

    aulas

---
+++ dom-01/ex.js
@@ -0,0 +1,9 @@
+// Retorne o url da página atual utilizando o objeto window
+const href = window.location.href;
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const ativo = document.querySelector(".ativo");
+// Retorne a linguagem do navegador
+const langNavigator = window.navigator.language;
+// Retorne a largura da janela
+const windowWidth = window.innerWidth;
